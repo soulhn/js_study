9c5e15bf61bd364f5e8d482c9d600536f3136c55
soulhn <vldnrlf123@naver.com>
2022-10-14 13:49:09 +0900

    notebook save

---
+++ applecodingJsWebUi/level2/tabMake.js
@@ -0,0 +1,12 @@
+for (let i = 0; i < 3; i++) {
+  $(".tab-button")
+    .eq(i)
+    .on("click", function () {
+      $(".tab-button").removeClass("orange");
+      $(".tab-button").eq(i).addClass("orange");
+      $(".tab-content").removeClass("show");
+      $(".tab-content").eq(i).addClass("show");
+    });
+}
+
+console.log(document.querySelectorAll(".tab-button").length);
